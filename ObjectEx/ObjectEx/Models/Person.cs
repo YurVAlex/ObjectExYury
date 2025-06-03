cd5b966fbe8f90b766fa01f047d0fb1721d2bbc8
Yura-va@tut.by <Yura-va@tut.by>
2025-06-03 21:59:45 +0300

    Homework10 created.

--- ObjectEx/ObjectEx/Models/Person.cs
+++ ObjectEx/ObjectEx/Models/Person.cs
@@ -13,42 +13,85 @@ public class Person
 
     public override bool Equals(object? obj)
     {
-        if (obj == null)
-        {
-            return false;
-        }
-
         if (ReferenceEquals(this, obj))
         {
             return true;
         }
-
+        if ((obj is null && this is not null) || (obj is not null && this is null))
+        {
+            return false;
+        }
+        if (obj is null && this is null) 
+        {
+            return true;
+        }
         if (obj is Person other)
         {
             return other.Name == Name && other.Age == Age;
         }
-
         return false;
     }
 
-    public static bool operator ==(Person a, Person b)
+    public override int GetHashCode()
     {
-        if (a.Name == b.Name && a.Age == b.Age)
+        var hashCode = int.MinValue;
+
+        foreach (var symbol in Name)
+        {
+            hashCode += (int)symbol + (Age % 2);
+        }
+        return hashCode;
+    }
+
+    public static bool operator ==(Person? a, Person? b)
+    {
+        if (ReferenceEquals(a, b))
         {
             return true;
         }
-        
-        return false;
-    }
-
-    public static bool operator !=(Person a, Person b)
-    {
-        if (a.Name != b.Name || a.Age != b.Age)
+        if ((a is null && b is not null) || (b is null && a is not null))
+        {
+            return false;
+        }
+        if (a is null && b is null)
         {
             return true;
         }
-
-        return false;
+        return a.Name == b.Name && a.Age == b.Age;
     }
 
+    public static bool operator !=(Person? a, Person? b)
+    {
+        if (ReferenceEquals(a, b))
+        {
+            return false;
+        }
+        if ((a is null && b is not null) || (b is null && a is not null))
+        {
+            return true;
+        }
+        if (a is null && b is null)
+        {
+            return false;
+        }
+        return a.Name != b.Name || a.Age != b.Age;
+    }
+
+    public static bool operator >(Person? a, Person? b)
+    {
+        if (a is null || b is null)
+        {
+           return false;
+        }
+        return a.Age > b.Age;
+    }
+
+    public static bool operator <(Person? a, Person? b)
+    {
+        if (a is null || b is null)
+        {
+            return false;
+        }
+        return a.Age < b.Age;
+    }
 }
--- ObjectEx/ObjectEx/Program.cs
+++ ObjectEx/ObjectEx/Program.cs
@@ -9,16 +9,42 @@ internal class Program
         var p1 = new Person { Name = "Yury", Age = 41 };
         var p2 = new Person { Name = "Alex", Age = 33 };
         var p3 = new Person { Name = "Yury", Age = 41 };
+        var p4 = new Person { Name = "Alex", Age = 50 };
 
+        Console.WriteLine($"p1: {p1}");
+        Console.WriteLine($"p2: {p2}");
+        Console.WriteLine($"p3: {p3}");
+        Console.WriteLine($"p4: {p4}");
+        Console.WriteLine();
 
-        Console.WriteLine(p1);
+        Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");
+        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
+        Console.WriteLine($"p2.Equals(p4): {p2.Equals(p4)}");
+        Console.WriteLine($"p2.Equals(p2): {p2.Equals(p2)}");
+        Console.WriteLine();
 
-        Console.WriteLine(p1.Equals(p2));
+        Console.WriteLine($"p1.GetHashCode(): {p1.GetHashCode()}");
+        Console.WriteLine($"p2.GetHashCode(): {p2.GetHashCode()}");
+        Console.WriteLine($"p3.GetHashCode(): {p3.GetHashCode()}");
+        Console.WriteLine($"p4.GetHashCode(): {p4.GetHashCode()}");
+        Console.WriteLine();
 
-        Console.WriteLine(p3.Equals(p1));
+        Console.WriteLine($"p1 == p2: {p1 == p2}");
+        Console.WriteLine($"p1 == p3: {p1 == p3}");
+        Console.WriteLine($"p1 != p2: {p1 != p2}");
+        Console.WriteLine($"p2 != p4: {p2 != p4}");
+        Console.WriteLine();
 
-        Console.WriteLine(p3 == p1);
+        Console.WriteLine($"p1 > p2: {p1 > p2}");
+        Console.WriteLine($"p2 < p1: {p2 < p1}");
+        Console.WriteLine($"p2 > p4: {p2 > p4}");
+        Console.WriteLine($"p4 > p1: {p4 > p1}");
+        Console.WriteLine();
 
+        Person older = p1 > p2 ? p1 : p2;
+        Console.WriteLine($"{older.Name} is older between p1 and p2.");
 
+        Person youngest = p1 < p2 ? p1 : p2;
+        Console.WriteLine($"{youngest.Name} is younger between p1 and p2.");
     }
 }
