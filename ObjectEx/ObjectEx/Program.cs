72c828384a157284fd8adfa4cb4d3467dea61e72
Yura-va@tut.by <Yura-va@tut.by>
2025-06-04 14:38:07 +0300

    Logic updated.

--- ObjectEx/ObjectEx/Program.cs
+++ ObjectEx/ObjectEx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ObjectEx.Models;
+using System.Net.NetworkInformation;
 
 namespace ObjectEx;
 
@@ -6,10 +7,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var p1 = new Person { Name = "Yury", Age = 41 };
-        var p2 = new Person { Name = "Alex", Age = 33 };
-        var p3 = new Person { Name = "Yury", Age = 41 };
-        var p4 = new Person { Name = "Alex", Age = 50 };
+        var p1 = new Person("Yury", 41);
+        var p2 = new Person("Alex", 33);
+        var p3 = new Person("Yury", 41);
+        var p4 = new Person("Alex", 50);
 
         Console.WriteLine($"p1: {p1}");
         Console.WriteLine($"p2: {p2}");
@@ -46,5 +47,23 @@ internal class Program
 
         Person youngest = p1 < p2 ? p1 : p2;
         Console.WriteLine($"{youngest.Name} is younger between p1 and p2.");
+
+        Console.WriteLine();
+        Console.WriteLine("p3 become null");
+        p3 = null;
+        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
+        Console.WriteLine($"p1 == p3: {p1 == p3}");
+        Console.WriteLine($"p1 != p3: {p1 != p3}");
+        Console.WriteLine($"p3 != p1: {p3 != p1}");
+        Console.WriteLine($"p1 > p3: {p1 > p3}");
+        Console.WriteLine($"p1 < p3: {p1 < p3}");
+
+        Console.WriteLine();
+        Console.WriteLine("p2 become null");
+        p2 = null;
+        Console.WriteLine($"p2 == p3: {p2 == p3}");
+        Console.WriteLine($"p2 != p3: {p2 != p3}");
+        Console.WriteLine($"p2 > p3: {p2 > p3}");
+        Console.WriteLine($"p2 < p3: {p2 < p3}");
     }
 }
