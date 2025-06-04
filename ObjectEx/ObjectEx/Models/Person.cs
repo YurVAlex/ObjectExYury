6758e00d72d408d443a9b4eec2a11b1e22eb33bf
Yura-va@tut.by <Yura-va@tut.by>
2025-06-04 14:52:38 +0300

    Updated. Ready.

--- ObjectEx/ObjectEx/Models/Person.cs
+++ ObjectEx/ObjectEx/Models/Person.cs
@@ -6,6 +6,67 @@ public class Person
 
     public int Age { get; set; }
 
+    public Person(string name, int age)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            Name = name;
+        }
+        else
+        {
+            Name = "Unknown";
+        }
+
+        if (age >= 0)
+        {
+            Age = age;
+        }
+        else
+        {
+            Age = -1;
+        }
+    }
+
+    public static bool operator ==(Person? a, Person? b)
+    {
+        if (ReferenceEquals(a, b))
+        {
+            return true;
+        }
+        if ((a is null && b is not null) || (b is null && a is not null)) // null == obj is false.
+        {
+            return false;
+        }
+        if (a is null && b is null) // null == null is true.
+        {
+            return true;
+        }
+        return a.Name == b.Name && a.Age == b.Age;
+    }
+
+    public static bool operator !=(Person? a, Person? b)
+    {
+        return !(a == b);
+    }
+
+    public static bool operator >(Person? a, Person? b)
+    {
+        if (ReferenceEquals(a, b))
+        {
+            return false;
+        }
+        if (a is null || b is null)
+        {
+            return false;
+        }
+        return a.Age > b.Age;
+    }
+
+    public static bool operator <(Person? a, Person? b)
+    {
+        return b > a;
+    }
+
     public override string ToString()
     {
         return $"Name: {Name}, Age: {Age}";
@@ -14,14 +75,6 @@ public class Person
     public override bool Equals(object? obj)
     {
         if (ReferenceEquals(this, obj))
-        {
-            return true;
-        }
-        if ((obj is null && this is not null) || (obj is not null && this is null))
-        {
-            return false;
-        }
-        if (obj is null && this is null) 
         {
             return true;
         }
@@ -34,64 +87,21 @@ public class Person
 
     public override int GetHashCode()
     {
-        var hashCode = int.MinValue;
+        var hashCode = Name != null ? Name.Length : 0;
 
-        foreach (var symbol in Name)
+        unchecked
         {
-            hashCode += (int)symbol + (Age % 2);
-        }
-        return hashCode;
-    }
+            if (hashCode != 0)
+            {
+                foreach (var symbol in Name)
+                {
+                    hashCode += (int)symbol * Name.Length;
+                }
+                hashCode += Age;
+            }
+            hashCode = ((Age % 2) == 0) ? hashCode * (-1) : hashCode;
 
-    public static bool operator ==(Person? a, Person? b)
-    {
-        if (ReferenceEquals(a, b))
-        {
-            return true;
+            return hashCode;
         }
-        if ((a is null && b is not null) || (b is null && a is not null))
-        {
-            return false;
-        }
-        if (a is null && b is null)
-        {
-            return true;
-        }
-        return a.Name == b.Name && a.Age == b.Age;
-    }
-
-    public static bool operator !=(Person? a, Person? b)
-    {
-        if (ReferenceEquals(a, b))
-        {
-            return false;
-        }
-        if ((a is null && b is not null) || (b is null && a is not null))
-        {
-            return true;
-        }
-        if (a is null && b is null)
-        {
-            return false;
-        }
-        return a.Name != b.Name || a.Age != b.Age;
-    }
-
-    public static bool operator >(Person? a, Person? b)
-    {
-        if (a is null || b is null)
-        {
-           return false;
-        }
-        return a.Age > b.Age;
-    }
-
-    public static bool operator <(Person? a, Person? b)
-    {
-        if (a is null || b is null)
-        {
-            return false;
-        }
-        return a.Age < b.Age;
-    }
+    } // Or proper: public override int GetHashCode() => HashCode.Combine(Name, Age);
 }
--- ObjectEx/ObjectEx/Program.cs
+++ ObjectEx/ObjectEx/Program.cs
@@ -6,10 +6,10 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var p1 = new Person { Name = "Yury", Age = 41 };
-        var p2 = new Person { Name = "Alex", Age = 33 };
-        var p3 = new Person { Name = "Yury", Age = 41 };
-        var p4 = new Person { Name = "Alex", Age = 50 };
+        var p1 = new Person("Yury", 41);
+        var p2 = new Person("Alex", 33);
+        var p3 = new Person("Yury", 41);
+        var p4 = new Person("Alex", 50);
 
         Console.WriteLine($"p1: {p1}");
         Console.WriteLine($"p2: {p2}");
@@ -46,5 +46,23 @@ public class Program
 
         Person youngest = p1 < p2 ? p1 : p2;
         Console.WriteLine($"{youngest.Name} is younger between p1 and p2.");
+
+        Console.WriteLine();
+        Console.WriteLine("p3 become null");
+        p3 = null;
+        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
+        Console.WriteLine($"p1 == p3: {p1 == p3}");
+        Console.WriteLine($"p1 != p3: {p1 != p3}");
+        Console.WriteLine($"p3 != p1: {p3 != p1}");
+        Console.WriteLine($"p1 > p3: {p1 > p3}");
+        Console.WriteLine($"p1 < p3: {p1 < p3}");
+
+        Console.WriteLine();
+        Console.WriteLine("p2 become null");
+        p2 = null;
+        Console.WriteLine($"p2 == p3: {p2 == p3}");
+        Console.WriteLine($"p2 != p3: {p2 != p3}");
+        Console.WriteLine($"p2 > p3: {p2 > p3}");
+        Console.WriteLine($"p2 < p3: {p2 < p3}");
     }
 }
